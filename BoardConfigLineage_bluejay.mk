3b923fcb3b42bbee517b40cc288677676dd3322a
Huy Tran <huytp.uit@gmail.com>
2025-04-13 17:16:49 +0700

    Update BoardConfigLineage_bluejay.mk

--- BoardConfigLineage_bluejay.mk
+++ BoardConfigLineage_bluejay.mk
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/akita/aam_compatibility_matrix.xml
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/akita/aam_compatibility_matrix.xml
 BOARD_AVB_ALGORITHM := SHA256_RSA4096
 BOARD_AVB_KEY_PATH := /home/huytp/.android-certs/releasekey.key
