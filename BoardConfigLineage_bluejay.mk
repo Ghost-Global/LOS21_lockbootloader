bca742e7c5456d084bbae2638ebdce6bf3df4334
Huy Tran <huytp.uit@gmail.com>
2025-04-13 15:38:00 +0700

    Update BoardConfigLineage_bluejay.mk

--- BoardConfigLineage_bluejay.mk
+++ BoardConfigLineage_bluejay.mk
@@ -1,8 +1,9 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-BOARD_AVB_ALGORITHM := SHA256_RSA4096
 
-BOARD_AVB_KEY_PATH := external/avb/test/data/avb_rsa4096.pem
+# DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/akita/aam_compatibility_matrix.xml
+BOARD_AVB_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_KEY_PATH := /home/huytp/.android-certs/releasekey.key
