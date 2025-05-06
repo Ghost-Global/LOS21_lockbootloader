6a0f03a4ce8daabe44ad46eb4524e161bd0a5913
Huy Tran <huytp.uit@gmail.com>
2025-05-06 07:26:02 +0700

    Update BoardConfigLineage_bluejay.mk

--- BoardConfigLineage_bluejay.mk
+++ BoardConfigLineage_bluejay.mk
@@ -6,4 +6,4 @@
 
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/akita/aam_compatibility_matrix.xml
 BOARD_AVB_ALGORITHM := SHA256_RSA4096
-BOARD_AVB_KEY_PATH := /home/huytp/.android-certs/releasekey.key
+BOARD_AVB_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
