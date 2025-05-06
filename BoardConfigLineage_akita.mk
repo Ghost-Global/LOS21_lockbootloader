160f0ba4652c68af0c6d58288d165045fc18a319
Huy Tran <huytp.uit@gmail.com>
2025-05-06 07:39:19 +0700

    Update BoardConfigLineage_akita.mk

--- BoardConfigLineage_akita.mk
+++ BoardConfigLineage_akita.mk
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/akita/aam_compatibility_matrix.xml
 BOARD_AVB_ALGORITHM := SHA256_RSA4096
 BOARD_AVB_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/akita/aam_compatibility_matrix.xml
+
